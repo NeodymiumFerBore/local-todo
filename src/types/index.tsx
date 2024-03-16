f14c2b65675d99d4943b405f4e94cceb1491ef72
NeodymiumFerBore <32781483+NeodymiumFerBore@users.noreply.github.com>
2024-03-16 20:03:39 +0100

    Simplify GUID class

--- src/types/index.tsx
+++ src/types/index.tsx
@@ -6,29 +6,16 @@ export class GUID {
 
   constructor(str?: string) {
     if (str === undefined) {
-      this.str = GUID.generate();
+      this.str = crypto.randomUUID();
     } else {
       if (GUID.validate(str)) this.str = str;
       else throw new Error("String '" + str + "' is not a valid GUID");
     }
+    console.log(this.str);
   }
 
   toString() {
     return this.str;
-  }
-
-  private static generate(): string {
-    // your favourite guid generation function could go here
-    // ex: http://stackoverflow.com/a/8809472/188246
-    let d = new Date().getTime();
-    if (window.performance && typeof window.performance.now === "function") {
-      d += performance.now(); //use high-precision timer if available
-    }
-    return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, (c) => {
-      let r = (d + Math.random() * 16) % 16 | 0;
-      d = Math.floor(d / 16);
-      return (c == "x" ? r : (r & 0x3) | 0x8).toString(16);
-    });
   }
 
   public static validate(guid: string): boolean {
