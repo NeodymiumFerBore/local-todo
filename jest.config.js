f1a5a921882b720153333197d2ebad164f8f0133
NeodymiumFerBore <32781483+NeodymiumFerBore@users.noreply.github.com>
2024-03-17 08:22:32 +0100

    Add jest coverage report

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,7 @@
 export default {
   preset: "ts-jest",
   testEnvironment: "jsdom",
+  collectCoverage: true,
+  // coverageReporters: ["json", "html", "text"],
+  coverageReporters: ["json", "text"],
 };
