5915ea7efe74271e09bba6a176f17312bf4179a4
NeodymiumFerBore <32781483+NeodymiumFerBore@users.noreply.github.com>
2024-03-17 09:59:08 +0100

    Add configurable base path in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,11 @@ import react from "@vitejs/plugin-react-swc";
 // Requires @types/node
 import path from "path";
 
+const base = process.env.BASE_PATH || "/";
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  base,
   plugins: [react()],
   esbuild: {
     drop: ["console", "debugger"],
