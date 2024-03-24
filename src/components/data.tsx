b1f20cbbb9966eacd45608206897b6fe344490f4
NeodymiumFerBore <32781483+NeodymiumFerBore@users.noreply.github.com>
2024-03-24 15:20:17 +0100

    data: replace quote ids with UUIDs

--- src/components/data.tsx
+++ src/components/data.tsx
@@ -38,64 +38,64 @@ export const authors: Author[] = [list1, list2, list3, list4];
 
 export const quotes: Quote[] = [
   {
-    id: "1",
+    id: "7343fe89-5119-4248-b5d3-59bcf13d0ed5",
     content: "Sometimes life is scary and dark",
     author: list2,
   },
   {
-    id: "2",
+    id: "34954a8a-8d75-4e72-bdb1-6dee4c5a2817",
     content:
       "Sucking at something is the first step towards being sorta good at something.",
     author: list1,
   },
   {
-    id: "3",
+    id: "1bd70895-7a61-4339-9a5e-a69c319b56f2",
     content: "You got to focus on what's real, man",
     author: list1,
   },
   {
-    id: "4",
+    id: "bd38f61b-0d8b-47f6-a227-47129642515d",
     content: "Is that where creativity comes from? From sad biz?",
     author: list3,
   },
   {
-    id: "5",
+    id: "d536c1fe-d778-4f26-b2d1-d8ae9c8db638",
     content: "Homies help homies. Always",
     author: list3,
   },
   {
-    id: "6",
+    id: "f741ae06-3c46-485a-8db7-9efef3a77b93",
     content: "Responsibility demands sacrifice",
     author: list4,
   },
   {
-    id: "7",
+    id: "f2e75823-e3f6-4e45-b43c-8c98d0bb3447",
     content: "That's it! The answer was so simple, I was too smart to see it!",
     author: list4,
   },
   {
-    id: "8",
+    id: "c4488a6b-eea9-499b-aa72-e0910926d0f4",
     content:
       "People make mistakes. It's all a part of growing up and you never really stop growing",
     author: list3,
   },
   {
-    id: "9",
+    id: "e43229b9-3451-4840-a163-43dd34736ad3",
     content: "Don't you always call sweatpants 'give up on life pants,' Jake?",
     author: list3,
   },
   {
-    id: "10",
+    id: "857b4c32-b515-4ea2-8841-94a158d3fd4e",
     content: "I should not have drunk that much tea!",
     author: list4,
   },
   {
-    id: "11",
+    id: "4c46eae3-f8c4-4ab4-aaa0-363b5890d34c",
     content: "Please! I need the real you!",
     author: list4,
   },
   {
-    id: "12",
+    id: "7e64f189-e70a-4ee0-846e-a4d7782280ee",
     content: "Haven't slept for a solid 83 hours, but, yeah, I'm good.",
     author: list4,
   },
