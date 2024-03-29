f6d8e17621d289ab6ef5f44224be2b2afb888963
NeodymiumFerBore <32781483+NeodymiumFerBore@users.noreply.github.com>
2024-03-29 22:41:02 +0100

    Clean App: remove unused functions, imports, states

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,9 @@
-import { useState } from "react";
 import "./styles.css";
-import { TodoListsContext } from "./contexts/Todo";
-import { TTodoItem, TTodoList, createId } from "./types";
 import { CssVarsProvider, Stack } from "@mui/joy";
-import { ThemeToggle } from "./components/ThemeToggle";
-import Board from "./components/Board";
 import { NavBar } from "./components/NavBar";
+import Board from "./components/Board";
 
 export default function App() {
-  // const [todos, setTodos] = useState(() => {
-  //   const localValue = localStorage.getItem("ITEMS");
-  //   if (localValue == null) return [];
-
-  //   return JSON.parse(localValue);
-  // });
-
-  const [todos, setTodos] = useState<TTodoItem[]>([]);
-  const [todoLists, setTodoLists] = useState<TTodoList[]>([]);
-
-  // useEffect(() => {
-  //   localStorage.setItem("ITEMS", JSON.stringify(todos));
-  // }, [todos]);
-
-  function addList() {
-    if (todoLists.length > 0) return;
-
-    const newList = {
-      id: createId(),
-      title: `List ${todoLists.length + 1}`,
-      description: "",
-      items: [],
-    };
-    setTodoLists((currentLists) => {
-      return [...currentLists, newList];
-    });
-  }
-
   return (
     <>
       <CssVarsProvider defaultMode="system">
