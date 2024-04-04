c61966b4c3b775c24efd9b2f1926f83a287e11c3
NeodymiumFerBore <32781483+NeodymiumFerBore@users.noreply.github.com>
2024-04-05 00:51:09 +0200

    usePersistentTabBoardsView: initial commit

--- src/components/TabBoardsView.tsx
+++ src/components/TabBoardsView.tsx
@@ -1,31 +1,96 @@
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { IconButton, Stack, TabList, TabPanel, Tabs, useTheme } from "@mui/joy";
 import { Add } from "@mui/icons-material";
 import { Id, TBoard, createId, newBoard } from "@/types";
 import Board from "./Board";
 import { EditableTab } from "./EditableTab";
+import { useLiveQuery } from "dexie-react-hooks";
+import { db } from "@/db";
+
+/**
+ * Do not handle selected tab with useLiveQuery. Selecting a tab would instantly
+ * select it on another tab, making it impossible to look at different boards
+ * at the same time. Use local storage or IDB, but do not watch for changes.
+ * Local storage may be easier and more adapted.
+ */
+const usePersistentTabBoardsView = (): [
+  boards: TBoard[],
+  selected: TBoard | undefined,
+  addBoard: (b?: TBoard) => void,
+  deleteBoard: (b: TBoard | Id) => void,
+  setSelected: (boardId: Id) => void
+] => {
+  const boards = useLiveQuery(
+    () => db.boards.orderBy("viewOrder").toArray(),
+    [],
+    []
+  );
+  const selected = useLiveQuery(() => db.boards.get({ selected: 1 }));
+  function setSelected(boardId: Id) {
+    console.log("Selecting board:", boardId);
+    db.transaction("rw", db.boards, async () => {
+      db.boards.where({ selected: 1 }).modify({ selected: 0 });
+      db.boards.where({ id: boardId }).modify({ selected: 1 });
+    });
+  }
+
+  function addBoard(b?: TBoard) {
+    const board = b || newBoard();
+    // const newBoardId = createId();
+    // Check length: Math.max will return -Infinity if used on an empty array
+    // const viewOrder =
+    board.viewOrder =
+      boards.length === 0
+        ? 1
+        : Math.max(...boards.map((item) => item.viewOrder)) + 1;
+    db.transaction("rw", db.boards, async () => {
+      db.boards.add(board);
+      // db.boards.add({
+      //   ...board,
+      //   viewOrder: viewOrder,
+      // });
+      // await setSelected(board);
+      // Unselect any selected board
+      // setSelected(board.id);
+      db.boards.where({ selected: 1 }).modify({ selected: 0 });
+      db.boards.where({ id: board.id }).modify({ selected: 1 });
+
+      // db.boards.where({ selected: 1 }).modify({ selected: 0 });
+    });
+  }
+
+  function deleteBoard(b: TBoard | Id) {
+    const boardId = typeof b === "string" ? b : b.id;
+    console.log("Removing board:", boardId);
+    db.transaction("rw", db.boards, async () => {
+      db.boards.delete(boardId);
+    });
+  }
+
+  // async function setSelected(b: TBoard | Id) {
+  //   const board: TBoard | undefined =
+  //     typeof b === "string" ? await db.boards.get({ id: b }) : b;
+  //   if (board === undefined) return;
+
+  //   db.transaction("rw", db.boards, async () => {
+  //     // db.boards.get(selected?.id).then((v) => (v!.selected = false));
+  //     await db.boards.where({ selected: 1 }).modify({ selected: 0 });
+  //     await db.boards.update({ id: board.id }, { selected: 1 });
+  //   }).then(() => console.log("setSelected: transaction complete!"));
+  // }
+
+  return [boards, selected, addBoard, deleteBoard, setSelected]; //, selected, setSelected];
+};
 
 export function TabBoardsView() {
   const theme = useTheme();
-  const [boards, setBoards] = useState<TBoard[]>([
-    {
-      id: crypto.randomUUID() as Id,
-      name: "Board 1",
-      description: "test description",
-      whenCreated: new Date(),
-      whenModified: new Date(),
-    },
-  ]);
-  const [selected, setSelected] = useState<Id | null>(
-    boards.length > 0 ? boards[0].id : null
-  );
+  // const [boards, addBoard, selected, setSelected] =
+  const [boards, selected, addBoard, deleteBoard, setSelected] =
+    usePersistentTabBoardsView();
 
   function renameBoard(name: string, boardId: Id) {
-    setBoards((curr) => {
-      return curr.map((e) => {
-        if (e.id === boardId) return { ...e, title: name };
-        else return e;
-      });
+    db.boards.where({ id: boardId }).modify((b) => {
+      b.name = name;
     });
   }
 
@@ -33,11 +98,13 @@ export function TabBoardsView() {
   return (
     <Tabs
       size="lg"
-      value={selected}
+      // value={selected?.id || null}
+      value={selected?.id || null}
       onChange={(_, v) => {
         // When deleting a tab, it also triggers a tab change.
         // Check if tab still exists before switching to it.
         if (boards.some((board) => board.id === v)) setSelected(v as Id);
+        // setSelected(v as Id);
       }}
     >
       <TabList
@@ -54,19 +121,22 @@ export function TabBoardsView() {
             <EditableTab
               key={board.id}
               value={board.id}
-              initialName={board.name}
+              initialName={board.id.slice(0, 6)}
               variant="plain"
               color="neutral"
+              // onDelete={() => deleteBoard(board.id)}
               onDelete={() => {
                 // If deleted tab is the currently selected, select another one if possible
-                if (selected === board.id && boards.length > 1) {
+                if (selected?.id === board.id && boards.length > 1) {
                   const i = boards.findIndex((e) => e.id === board.id);
                   // Try to select next tab. Visually, it looks like selection stays in place
                   setSelected(
                     i < boards.length - 1 ? boards[i + 1].id : boards[i - 1].id
                   );
                 }
-                setBoards((curr) => curr.filter((e) => e.id !== board.id));
+                deleteBoard(board.id);
+                // db.boards.where({ id: board.id }).delete();
+                // setBoards((curr) => curr.filter((e) => e.id !== board.id));
               }}
               onRename={(s) => renameBoard(s, board.id)}
               sx={{
@@ -76,17 +146,7 @@ export function TabBoardsView() {
             />
           );
         })}
-        <IconButton
-          key={crypto.randomUUID()}
-          onClick={() => {
-            const newBoardId = createId();
-            setBoards((curr) => [
-              ...curr,
-              newBoard({ id: newBoardId, name: "New Board" }),
-            ]);
-            setSelected(newBoardId);
-          }}
-        >
+        <IconButton key={crypto.randomUUID()} onClick={() => addBoard()}>
           <Add />
         </IconButton>
       </TabList>
